94763d7c83a47e086995cd41ea0ba722fa9c888b
Ashish <iashishyadav17@gmai.com>
2025-02-07 14:26:54 +0530

    updated

--- Backend/src/middleware/catchAsyncError.js
+++
@@ -1,8 +0,0 @@
-const ErrorHandler = require("..src/Utils/ErrorHandler");
-const catchAsyncErrors = require("./catchAsyncErrors");
-const jwt = require("jsonwebtoken");
-=======
-module.exports = (theFunc) => (req, res, next) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next);
-  };
-
